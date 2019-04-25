fa1b27bee479a2ccbbe883fc5a3f6add8d32d966
Your Name <you@example.com>
2019-04-25 21:30:10 +0200

    first upload

---
+++ form.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Eng Break</title>
+	<link rel="stylesheet" type="text/css" href="styleform.css">
+	<meta name="viewport" content="width=device-width">
+
+</head>
+<body>
+<div class=" wrapper divform">
+<form class=" form">
+<label for="name">Name</label>
+<br>
+<input type="text" id="name" required>
+<br>
+<label for="phone">Phone</label>
+<br>
+<input type="number" name="" id="phone" required>
+<br>
+<label for="email">Email</label>
+<br>
+<input type="email" name="" id="email" required>
+<br>
+<label for="acy">Academic Year</label>
+<br>
+<input type="text" name="" id="acy" required>
+<br>
+<label for="uni">University/Faculty</label>
+<br>
+<input type="text" name="" id="uni" required>
+<br>
+<input type="submit" name="" >
+</form>
+
+</body>
+</html>
